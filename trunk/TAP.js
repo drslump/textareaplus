d6eb191316ced9d900363c59a004b76e1d867123
drSlump80 <drSlump80@78358e81-ab19-0410-99b8-477aee2c4887>
2007-11-03 20:31:49 +0000

    Initial commit of the new version

---
+++ trunk/TAP.js
@@ -0,0 +1,44 @@
+/* $Id$
+  
+Script: TAP.js
+
+    TextAreaPlus 0.1 - A source code text editor in Javascript
+
+License:
+
+    The GNU General Public License version 3 (GPLv3)
+    
+    This file is part of TextArea+.
+
+    TextArea+ is free software; you can redistribute it and/or modify it under 
+    the terms of the GNU General Public License as published by the Free Software
+    Foundation; either version 2 of the License, or (at your option) any later 
+    version.
+    
+    TextArea+ is distributed in the hope that it will be useful, but WITHOUT ANY 
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+    A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License along with 
+    TextArea+; if not, write to the Free Software Foundation, Inc., 51 Franklin 
+    Street, Fifth Floor, Boston, MA 02110-1301, USA
+    
+    See bundled license.txt or check <http://www.gnu.org/copyleft/gpl.html>
+
+Copyright:
+    
+    copyright (c) 2005, 2006, 2007 Ivan Montes <http://blog.netxus.es>
+*/
+
+/*
+Class: TAP
+    TextAreaPlus main class and namespace container
+
+Example:
+	(start code)
+	var editor = new TAP();
+	(end)
+*/
+function TAP() {
+    
+}
